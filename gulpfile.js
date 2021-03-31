6282a1d177ef73fef80684bb1aa4fe375734ca66
Olga Solovyeva <ezhicc@gmail.com>
2021-03-31 23:00:30 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+
+    var gulp = require('gulp');
+    var sass = require('gulp-sass');
+
+    gulp.task('sass', function (){
+        return gulp.src('src/scss/main.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('src/css'))
+    });
